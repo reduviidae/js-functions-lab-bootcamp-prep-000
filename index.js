5f7f942620203aeaa769d420c2f431b554b653cb
Wil Cook <wil.cook.is@gmail.com>
2018-10-17 19:37:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays ${name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday} to ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} until ${holiday}!`
+  return `It\'s ${days} until ${holiday}!`
 }
